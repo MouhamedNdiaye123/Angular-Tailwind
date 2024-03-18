892cf2c6e56d184c9f666f2ca212c9daebc1b439
Mouhamed ndiaye <Mouhamethndiaye123@gmail.com>
2024-03-18 15:58:43 +0000

    ajout component avis et update homeComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AvisComponent } from '../avis/avis.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AvisComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
